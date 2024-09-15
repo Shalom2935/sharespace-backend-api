f19c02dd0f2c60450e4394c14f1b08976f0a7fe0
Shalom <kamokoueshalom@gmail.com>
2024-09-15 05:10:22 +0200

    access secret from gcr

--- src/config/gcsConfig.js
+++ src/config/gcsConfig.js
@@ -1,10 +1,15 @@
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
 
-// Initialize Google Cloud Storage Client
+// Parse the credentials from the environment variable (GCS_CREDENTIALS)
+const credentials = JSON.parse(process.env.GCS_CREDENTIALS);
+
+// Initialize Google Cloud Storage Client using the parsed credentials
 const storage = new Storage({
-    keyFilename: path.join(__dirname, '../../sjp-share-space-keyfile.json'),
-    projectId: process.env.GCP_PROJECT_ID
+    projectId: process.env.GCP_PROJECT_ID, // Set your project ID from env
+    credentials: {
+        client_email: credentials.client_email,
+        private_key: credentials.private_key
+    }
 });
 
 // Bucket reference
